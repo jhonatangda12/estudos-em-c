ec62d37d92097e4d8e5162296ae032060782d068
Jhonatan B Barbosa <90397151+jhonatangda12@users.noreply.github.com>
2025-09-22 18:26:01 -0300

    new

---
+++ Módulo 3 de Introdução a programação/switchDentrodoSwitch.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+int jogo;
+int numeroSecreto;
+int palpite;
+
+void Menu(){
+  printf("Menu principal\n");
+  printf("1. Iniciar jogo\n");
+  printf("2. Ver regras\n");
+  printf("3. Sair\n");
+  printf("Escolha sua opção: ");
+  scanf("%d", &jogo);
+}
+
+void Condicao(){
+  switch (jogo)
+  {
+  case 1 :
+    srand(time(0)); // inicializar gerador de números aleatórios
+    numeroSecreto = rand() % 10;
+    printf("Escolha um número (0 a 9): ");
+    scanf("%d", &palpite);
+    if (numeroSecreto == palpite) {
+    printf("Você acertou!\n");
+    printf("Número gerado: %d\n", numeroSecreto);
+    }
+    else if (palpite > 9)
+    {
+      printf("Número inválido!");
+    }
+    else {
+    printf("Você errou!\n");
+    printf("Número gerado: %d\n", numeroSecreto);
+    }
+    break;
+    
+    default:
+        break;
+    }
+}
+int main(){
+  Menu();
+  Condicao();
+  return 0;
+}
