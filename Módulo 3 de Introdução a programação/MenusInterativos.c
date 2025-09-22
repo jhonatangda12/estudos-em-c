81a64390e339e1d378ecc02565e96a2d1d64f265
Jhonatan B Barbosa <90397151+jhonatangda12@users.noreply.github.com>
2025-09-22 14:33:44 -0300

    New

--- Módulo 3 de Introdução a programação/MenusInterativos.c
+++ Módulo 3 de Introdução a programação/MenusInterativos.c
@@ -1,1 +1,35 @@
-
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+int opcao;
+void Menu(){
+  printf("Menu principal\n");
+  printf("1. Iniciar jogo\n");
+  printf("2. Ver regras\n");
+  printf("3. Sair\n");
+  printf("Escolha sua opção: ");
+  scanf("%d", &opcao);
+}
+void Condicao(){
+  switch (opcao)
+  {
+  case 1 :
+    printf("Execução do jogo!\n");
+    break;
+  case 2 :
+    printf("Regras...");
+    break;
+  case 3 :
+    printf("Saindo do jogo!");
+    break;        
+  
+  default:
+    printf("Opção inválida!");  
+    break;
+  }
+}
+int main(){
+  Menu();
+  Condicao();
+  return 0;
+}
